cd3d87367233748804e89ea9950d79fb6be6658d
nbrucepro <nezabruce@gmail.com>
2023-05-30 15:59:55 +0200

    [ft]-signup-screen and navigator-component

---
+++ src/components/navigation/Navigator.jsx
@@ -0,0 +1,145 @@
+import { AntDesign, MaterialCommunityIcons } from "@expo/vector-icons";
+import {
+  CardStyleInterpolators,
+  createStackNavigator,
+} from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import * as SecureStore from "expo-secure-store";
+import React from "react";
+import Signup from "../../screens/Signup";
+import { TouchableOpacity, View } from "react-native";
+
+export default Navigator = () => {
+  return <AppNavigator />;
+};
+
+const AuthNavigator = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator
+      initialRouteName="SignIn"
+      screenOptions={{
+        gestureEnabled: true,
+        gestureDirection: "horizontal",
+        cardStyleInterpolator: CardStyleInterpolators,
+      }}
+    >
+      <Stack.Screen
+        name="SignIn"
+        component={Login}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="SignUp"
+        component={Signup}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+const Tabs = createBottomTabNavigator();
+
+const AppNavigator = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const getToken = async () => {
+      const token = await SecureStore.getItemAsync("token");
+      if (token) {
+        setIsAuthenticated(true);
+      }
+    };
+    getToken();
+  }, []);
+
+  if (!isAuthenticated) return <AuthNavigator />;
+
+  return (
+    <Tabs.Navigator
+      initialRouteName="Scan"
+      screenOptions={{
+        tabBarHideOnKeyboard: true,
+        headerShown: false,
+        tabBarInactiveTintColor: "black",
+        tabBarStyle: {
+          backgroundColor: "white",
+          height: 80,
+          paddingBottom: 10,
+          padding: 30,
+          borderTopLeftRadius: 30,
+          borderTopRightRadius: 30,
+          marginHorizontal: 5,
+          position: "absolute",
+          borderColor: "white",
+          elevation: 10,
+        },
+        tabBarButton: (props) => {
+          return (
+            <View {...props}>
+              <View
+                style={{
+                  minWidth: 50,
+                  minHeight: 50,
+                  borderRadius: 10,
+                  backgroundColor: props.accessibilityState.selected
+                    ? "#F6E3DB"
+                    : "white",
+                }}
+              >
+                <TouchableOpacity {...props} />
+              </View>
+            </View>
+          );
+        },
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: "#F7941D",
+      }}
+    >
+      <Tabs.Screen
+        name="Home"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="home" size={24} color={color} />
+          ),
+        }}
+        component={DashBoardScreen}
+      />
+      <Tabs.Screen
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="bell-badge-outline"
+              size={24}
+              color={color}
+            />
+          ),
+        }}
+        name="Notification"
+        component={NotificationScreen}
+      />
+      <Tabs.Screen
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="progress-clock"
+              size={24}
+              color={color}
+            />
+          ),
+        }}
+        name="Clock"
+        component={SearchStack}
+      />
+      <Tabs.Screen
+        options={{
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="shoppingCart" size={24} color={color} />
+          ),
+        }}
+        name="Cart"
+        component={ShoppingCartScreen}
+      />
+    </Tabs.Navigator>
+  );
+};
--- src/screens/Signup.jsx
+++ src/screens/Signup.jsx
@@ -1,9 +1,251 @@
-import React from 'react'
+import { useFormik } from "formik";
+import React from "react";
+import {
+  Alert,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import colors from "../util/colors";
+import Icon from "react-native-vector-icons/Feather";
 
-const Signup = () => {
+const Signup = ({ navigation }) => {
+  const { handleSubmit, handleChange, values } = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      mobile: "",
+      fullName: "",
+    },
+    onSubmit: async (values, { resetForm }) => {
+      if (
+        !values.email ||
+        !values.password ||
+        !values.mobile ||
+        !values.fullName
+      ) {
+        Alert.alert("Error", "you must fill in all fields");
+        return;
+      }
+      const response = await fetch("", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: values.email,
+          password: values.password,
+          mobile: values.mobile,
+          firstName: values.fullName.split(" ")[0],
+          lastName: values.fullName.split(" ")[1],
+        }),
+      });
+      const data = await response.json();
+      if (!data) {
+        console.log("");
+      } else {
+        Alert.alert("Success", "you've successfully signed up!");
+        resetForm();
+        // navigation.navigate('Signin')
+      }
+    },
+  });
   return (
-    <div>Signup</div>
-  )
-}
+    <SafeAreaView style={styles.container}>
+      <View style={styles.form}>
+        <View style={styles.logo}>
+          <Text style={styles.logoText}>Supa</Text>
+          <Text style={styles.logoTextM}>Menu</Text>
+        </View>
+        <View style={styles.formText}>
+          <Text style={styles.formTextWelcome}>Welcome.</Text>
+          <Text style={styles.formTextFill}>
+            Please Fill in the information
+          </Text>
+        </View>
+        <View style={styles.inputsArea}>
+          <View>
+            <TextInput
+              style={styles.input}
+              placeholder="Fullname"
+              autoCapitalize="none"
+              value={values.fullName}
+              onChangeText={handleChange("fullName")}
+            />
+            <Icon name="user" size={20} color="#9098b2" style={styles.icon} />
+          </View>
+          <View>
+            <Icon name="phone" size={20} color="#9098b2" style={styles.icon} />
+            <TextInput
+              style={styles.input}
+              placeholder="Phone Number"
+              autoCapitalize="none"
+              value={values.mobile}
+              onChangeText={handleChange("mobile")}
+            />
+          </View>
+          <View>
+            <Icon name="mail" size={20} color="#9098b2" style={styles.icon} />
+            <TextInput
+              style={styles.input}
+              placeholder="Your email"
+              autoCapitalize="none"
+              value={values.email}
+              onChangeText={handleChange("email")}
+            />
+          </View>
+          <View>
+            <Icon name="lock" size={20} color="#9098b2" style={styles.icon} />
+            <TextInput
+              style={styles.input}
+              placeholder="Your password"
+              autoCapitalize="none"
+              value={values.password}
+            />
+          </View>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              handleSubmit();
+            }}
+          >
+            <Text style={styles.buttonText}>Proceed</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.horizontalContainer}>
+          <View style={styles.horizontalLine} />
+          <Text style={styles.or}>OR</Text>
+          <View style={styles.horizontalLine} />
+        </View>
+        <Text style={styles.haveAnAccount}>If you have PMG account</Text>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            // navigation.navigate("signin");
+            console.log('go to sign in');
+          }}
+        >
+          <Text style={styles.buttonText}>Sign in</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default Signup
+export default Signup;
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: colors.default,
+    borderRadius: 5,
+    elevation: 6,
+    padding: "5%",
+    shadowColor: "rgba(0,0,0,0.1)",
+    shadowOffset: { width: 1, height: 13 },
+    shadowOpacity: 0.8,
+    shadowRadius: 15,
+  },
+  buttonText: {
+    color: colors.white,
+    fontFamily: "Roboto_700Bold",
+    fontSize: 15,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  container: {
+    backgroundColor: "#F7941D",
+    height: "100%",
+    width: "100%",
+  },
+  dontHaveAccount: {
+    color: "#9098b2",
+    fontSize: 15,
+    paddingVertical: "5%",
+    textAlign: "center",
+  },
+  form: {
+    backgroundColor: colors.white,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    height: "100%",
+    marginTop: "20%",
+    padding: 20,
+  },
+  formTextFill: {
+    color: "#9098b2",
+    fontSize: 15,
+    paddingBottom: "1%",
+  },
+  formText: {
+    alignItems: "center",
+    display: "flex",
+    justifyContent: "center",
+    marginTop: "5%",
+  },
+  formTextWelcome: {
+    color: "#9098b2",
+    fontFamily: "Roboto_700Bold",
+    fontSize: 20,
+    fontWeight: "bold",
+    paddingBottom: "1%",
+  },
+  haveAnAccount: {
+    color: "9098b2",
+    fontSize: 15,
+    paddingVertical: "5%",
+    textAlign: "center",
+  },
+  horizontalContainer: {
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  horizontalLine: {
+    borderBottomColor: colors.border,
+    borderBottomWidth: 1,
+    flex: 1,
+    height: 1,
+  },
+  icon: {
+    left: 12,
+    position: "absolute",
+    top: 18,
+  },
+  input: {
+    borderColor: colors.border,
+    borderRadius: 5,
+    borderWidth: 1,
+    marginBottom: 20,
+    padding: "5%",
+    paddingLeft: 40,
+  },
+  inputsArea: {
+    display: "flex",
+    paddingVertical: "10%",
+  },
+  logo: {
+    alignItems: "center",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  logoText: {
+    fontFamily: "Roboto_700Bold",
+    fontSize: 40,
+    fontWeight: "bold",
+  },
+  logoTextM: {
+    color: colors.default,
+    fontFamily: "Roboto_700Bold",
+    fontSize: 40,
+    fontWeight: "bold",
+    marginLeft: 3,
+  },
+  or: {
+    color: colors.border,
+    textAlign: "center",
+    width: 50,
+  },
+});
